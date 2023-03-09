f22849a7a44afc479c17228eefa13736df2abfcf
Pavel Bondarevskii <gohre94@gmail.com>
2023-03-09 11:32:29 +0300

    rename localhost

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const { PORT = 3000 } = process.env;
 mongoose.set('strictQuery', true);
 mongoose
-  .connect('mongodb://127.0.0.1:27017/mestodb')
+  .connect('mongodb://localhost:27017/mestodb')
   .then(() => {
     console.log('connected');
   })
